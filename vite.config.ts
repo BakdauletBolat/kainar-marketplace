04b4d39ec12179334f175e2f3280f3103366b6bf
Бакдаулет Болат <bolat@Bakdaulets-MacBook-Air.local>
2024-04-28 14:37:31 +0500

    asdas

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//@ts-ignore
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -7,6 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      //@ts-ignore
       '@': path.resolve(__dirname, 'src'),
     },
   }
